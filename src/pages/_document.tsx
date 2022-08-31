9326e4d8cae0e75dddd6cd3c609860ec48b19590
Matheus Pereira <matheus@stargrid.pro>
2022-08-30 22:08:20 -0300

    criando _document

---
+++ src/pages/_document.tsx
@@ -0,0 +1,15 @@
+import { Html, Head, Main, NextScript } from 'next/document'
+
+export default function document() {
+    return (
+        <Html>
+            <Head>
+
+            </Head>
+            <body>
+                <Main />
+                <NextScript />
+            </body>
+        </Html>
+    )
+}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,16 @@
 import type { NextPage } from 'next'
-
+import Head from 'next/head'
 
 const Home: NextPage = () => {
   return (
+    <>
+    <Head>
+      <title>Pizzaria - Login</title>
+    </Head>
     <div>
       <h1>Pizzaria</h1>
     </div>
+    </>
   )
 }
 
