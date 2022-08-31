44f1b65b4246e4f8e4ce498d5666d90b36b7829d
Matheus Pereira <matheus@stargrid.pro>
2022-08-31 00:08:20 -0300

    tela de login

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,17 +1,46 @@
-import type { NextPage } from 'next'
-import Head from 'next/head'
+import Image from 'next/image'
 
-const Home: NextPage = () => {
+import Head from 'next/head'
+import styles from '../../styles/home.module.scss'
+
+import logoImg from '../../public/logo.svg'
+import { Input } from '../components/ui/Input'
+import { Button } from '../components/ui/Button'
+
+export default function() {
   return (
     <>
     <Head>
       <title>Pizzaria - Login</title>
     </Head>
-    <div>
-      <h1>Pizzaria</h1>
+    <div className={styles.containerCenter}>
+      <Image src={logoImg} alt="Logo Pizzaria"/>
+
+      <div className={styles.login}>
+        <form>
+          <Input 
+            placeholder='Digite seu email'
+            type="text"
+          />
+
+          <Input 
+            placeholder='Digite sua senha'
+            type="password"
+          />
+
+          <Button 
+            type="submit"
+            loading={false}
+          >
+            Login
+          </Button>
+        </form>
+
+        <a className={styles.text}>Não possui uma conta? Cadastre-se</a>
+
+      </div>
     </div>
     </>
   )
 }
 
-export default Home
